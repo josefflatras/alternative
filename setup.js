2345a5c7a03f461f1167faf5f4cd0f4ffe641a5a
joseff <josefflatras@gmail.com>
2025-09-07 17:56:11 -0400

    hotfix 9.7.2025

--- setup.js
+++ setup.js
@@ -1,4 +1,4 @@
-import { PageFlip } from './node_modules/page-flip/dist/js/page-flip.module.js';
+import { PageFlip } from './lib/page-flip.module.js';
 
 /*
 const pageFlip = new PageFlip(document.getElementById('book'),
